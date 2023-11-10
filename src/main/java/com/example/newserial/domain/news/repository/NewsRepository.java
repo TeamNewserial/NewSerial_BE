fde2f7cda2ffb1190f80d19c75393bb08ae302d3
aaa67 <114573522+aaa67@users.noreply.github.com>
2023-11-10 15:23:15 +0900

    Revert "[Feat] 뉴스 본문 패러프레이징 기능"

--- src/main/java/com/example/newserial/domain/news/repository/NewsRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.newserial.domain.news.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NewsRepository extends JpaRepository<News, Long> {
-}
