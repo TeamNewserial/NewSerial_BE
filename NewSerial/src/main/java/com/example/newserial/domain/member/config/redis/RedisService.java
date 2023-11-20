15d9786af8cf7d3c3048bc8c19f284d245bacee4
Jihye Lee <zerom980706@gmail.com>
2023-11-20 21:40:58 +0900

    feat(Security): 레디스 관련 메서드 전체수정

--- NewSerial/src/main/java/com/example/newserial/domain/member/config/redis/RedisService.java
+++ NewSerial/src/main/java/com/example/newserial/domain/member/config/redis/RedisService.java
@@ -3,6 +3,7 @@ package com.example.newserial.domain.member.config.redis;
 import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,28 +12,40 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class RedisService {
 
-    private final RedisTemplate<String, String> redisTemplate;
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final RedisTemplate<String, Object> redisBlackListTemplate;
 
-    //키-값 저장
-    @Transactional
-    public void setValues(String key, String value){
-        redisTemplate.opsForValue().set(key, value);
+    public void set(String key, Object o, int minutes) {
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer(o.getClass()));
+        redisTemplate.opsForValue().set(key, o, minutes, TimeUnit.MINUTES);
     }
 
-    // 키-값을 만료시간과 함께 저장
-    @Transactional
-    public void setValuesWithTimeout(String key, String value, long timeout){
-        redisTemplate.opsForValue().set(key, value, timeout, TimeUnit.MILLISECONDS);
-    }
-
-    //키를 이용해 값 구하기
-    public String getValues(String key){
+    public Object get(String key) {
         return redisTemplate.opsForValue().get(key);
     }
 
-    //키를 이용해 값 삭제
-    @Transactional
-    public void deleteValues(String key) {
-        redisTemplate.delete(key);
+    public boolean delete(String key) {
+        return Boolean.TRUE.equals(redisTemplate.delete(key));
+    }
+
+    public boolean hasKey(String key) {
+        return Boolean.TRUE.equals(redisTemplate.hasKey(key));
+    }
+
+    public void setBlackList(String key, Object o, int minutes) {
+        redisBlackListTemplate.setValueSerializer(new Jackson2JsonRedisSerializer(o.getClass()));
+        redisBlackListTemplate.opsForValue().set(key, o, minutes, TimeUnit.MINUTES);
+    }
+
+    public Object getBlackList(String key) {
+        return redisBlackListTemplate.opsForValue().get(key);
+    }
+
+    public boolean deleteBlackList(String key) {
+        return Boolean.TRUE.equals(redisBlackListTemplate.delete(key));
+    }
+
+    public boolean hasKeyBlackList(String key) {
+        return Boolean.TRUE.equals(redisBlackListTemplate.hasKey(key));
     }
 }
---
+++ NewSerial/src/main/java/com/example/newserial/domain/member/service/AuthService.java
@@ -0,0 +1,2 @@
+package com.example.newserial.domain.member.service;public class AuthService {
+}
