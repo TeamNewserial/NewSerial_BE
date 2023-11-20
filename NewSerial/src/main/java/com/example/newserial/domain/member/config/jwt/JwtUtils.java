1325df98eb168739736a13a0d20fa4acbecc4246
Jihye Lee <zerom980706@gmail.com>
2023-11-20 21:51:30 +0900

    feat(Security): accessToken, refreshToken 생성 메서드 추가, validate 로직 수정(로그아웃용)

--- NewSerial/src/main/java/com/example/newserial/domain/member/config/jwt/JwtUtils.java
+++ NewSerial/src/main/java/com/example/newserial/domain/member/config/jwt/JwtUtils.java
@@ -1,5 +1,8 @@
 package com.example.newserial.domain.member.config.jwt;
 
+import com.example.newserial.domain.error.BadRequestException;
+import com.example.newserial.domain.error.ErrorCode;
+import com.example.newserial.domain.member.config.redis.RedisService;
 import com.example.newserial.domain.member.config.services.UserDetailsImpl;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
@@ -10,6 +13,7 @@ import io.jsonwebtoken.io.Decoders;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.Date;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,21 +24,26 @@ import java.security.Key;
 import io.jsonwebtoken.security.Keys;
 
 @Component
+@RequiredArgsConstructor
 public class JwtUtils {
     private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+    private final RedisService redisService;
 
     @Value("${jwtSecret}")
     private String jwtSecret;
 
-    @Value("${jwtExpirationMs}")
-    private int jwtExpirationMs;
+    @Value("${jwtExpirationMsAT}")
+    private int ATExpirationMs;
 
-    @Value("${jwtCookieName}")
-    private String jwtCookie;
+    @Value("${jwtExpirationMsRT}")
+    private int RTExpirationMs;
+
+    @Value("${jwtCookieNameRT}")
+    private String RTCookie;
 
     //쿠키에서 토큰 꺼내기
     public String getJwtFromCookies(HttpServletRequest request) {
-        Cookie cookie = WebUtils.getCookie(request, jwtCookie);
+        Cookie cookie = WebUtils.getCookie(request, RTCookie);
         if (cookie != null) {
             return cookie.getValue();
         } else {
@@ -42,16 +51,17 @@ public class JwtUtils {
         }
     }
 
-    //토큰 저장한 쿠키 생성
-    public ResponseCookie generateJwtCookie(UserDetailsImpl userPrincipal) {
-        String jwt = generateTokenFromEmail(userPrincipal.getEmail());
-        ResponseCookie cookie = ResponseCookie.from(jwtCookie, jwt).path("/").maxAge(24*60*60).httpOnly(true).build();
-        return cookie;
+    //토큰 저장한 쿠키 생성(+RT - Secure, HttpOnly)
+    public ResponseCookie generateRefreshTokenCookie(UserDetailsImpl userPrincipal) {
+        String RefreshToken = generateRefreshTokenFromEmail(userPrincipal.getEmail());
+        //https 사용시 secure 설정 true
+        ResponseCookie Rtcookie = ResponseCookie.from(RTCookie, RefreshToken).path("/").maxAge(24*60*60).httpOnly(true).build();
+        return Rtcookie;
     }
 
     //쿠키 삭제
     public ResponseCookie getCleanJwtCookie() {
-        ResponseCookie cookie = ResponseCookie.from(jwtCookie, null).path("/").build();
+        ResponseCookie cookie = ResponseCookie.from(RTCookie, null).path("/").build();
         return cookie;
     }
 
@@ -70,6 +80,9 @@ public class JwtUtils {
     public boolean validateJwtToken(String authToken) {
         try {
             Jwts.parserBuilder().setSigningKey(key()).build().parse(authToken);
+            if (redisService.hasKeyBlackList(authToken)) {
+                throw new BadRequestException("로그아웃한 상태입니다.", ErrorCode.BAD_REQUEST);
+            }
             return true;
         } catch (MalformedJwtException e) {
             logger.error("Invalid JWT token: {}", e.getMessage());
@@ -80,24 +93,29 @@ public class JwtUtils {
         } catch (IllegalArgumentException e) {
             logger.error("JWT claims string is empty: {}", e.getMessage());
         }
-
         return false;
     }
 
 
-    //이메일을 이용해 토큰 생성
-    public String generateTokenFromEmail(String email) {
+    //TODO accessToken, refreshToken 생성
+    //accessToken 발급
+    public String generateAccessTokenFromEmail(String email) {
         return Jwts.builder()
                 .setSubject(email)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .setExpiration(new Date((new Date()).getTime() + ATExpirationMs))
                 .signWith(key(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
-    //TODO accessToken, refreshToken 생성
+    //refreshToken 발급
+    public String generateRefreshTokenFromEmail(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + RTExpirationMs))
+                .signWith(key(), SignatureAlgorithm.HS256)
+                .compact();
+    }
 
-    //TODO accessToken, refreshToken 검증
-
-    //
 }
