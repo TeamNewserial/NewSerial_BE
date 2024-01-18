1121fdd245ffa53833b1a0ad1c2d3432653998a3
Jihye Lee <zerom980706@gmail.com>
2024-01-19 02:20:43 +0900

    fix(cors): @CrossOrigin 추가

--- NewSerial/src/main/java/com/example/newserial/WebConfig.java
+++ NewSerial/src/main/java/com/example/newserial/WebConfig.java
@@ -1,17 +1,17 @@
-//package com.example.newserial;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("http://localhost:8080", "http://localhost:3000")
-//                .allowedMethods("GET", "POST", "PUT", "DELETE")
-//                .allowedHeaders("Authorization", "content-type")  // 이 부분을 추가
-//                .allowCredentials(true);  // 필요시 credentials 허용
-//    }
-//}
+package com.example.newserial;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080", "http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "content-type")  // 이 부분을 추가
+                .allowCredentials(true);  // 필요시 credentials 허용
+    }
+}
--- NewSerial/src/main/java/com/example/newserial/domain/quiz/controller/QuizController.java
+++ NewSerial/src/main/java/com/example/newserial/domain/quiz/controller/QuizController.java
@@ -12,11 +12,13 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true", allowedHeaders = "Authorization")
 @RestController
 public class QuizController {
 
