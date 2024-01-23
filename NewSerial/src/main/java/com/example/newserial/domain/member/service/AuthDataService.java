79e92bcae671d52c116155d527a3fbad0ae42011
Elenaljh <zerom980706@gmail.com>
2024-01-23 21:07:01 +0900

    fix(redirection): 토큰만료시 홈으로 가게 리다이렉션

--- NewSerial/src/main/java/com/example/newserial/domain/member/service/AuthDataService.java
+++ NewSerial/src/main/java/com/example/newserial/domain/member/service/AuthDataService.java
@@ -70,7 +70,7 @@ public class AuthDataService {
     // 로그인으로 리다이렉션
     public ResponseEntity<?> redirectToLogin() {
         return ResponseEntity.status(HttpStatus.SEE_OTHER)
-                .header("Location", "/home")
+                .header("Location", "/")
                 .build();
     }
 
