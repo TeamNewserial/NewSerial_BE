9165aa23b867bd444589b3df465b4af2862822c0
Jihye Lee <zerom980706@gmail.com>
2024-01-30 23:04:23 +0900

    fix(socialLogin): 리다이렉션 문제 해결

--- NewSerial/src/main/java/com/example/newserial/domain/member/config/oauth2/handler/OAuth2LoginSuccessHandler.java
+++ NewSerial/src/main/java/com/example/newserial/domain/member/config/oauth2/handler/OAuth2LoginSuccessHandler.java
@@ -3,26 +3,25 @@ package com.example.newserial.domain.member.config.oauth2.handler;
 import com.example.newserial.domain.member.config.jwt.JwtUtils;
 import com.example.newserial.domain.member.config.jwt.TokenCarrier;
 import com.example.newserial.domain.member.config.oauth2.CustomOAuth2User;
-import com.example.newserial.domain.member.repository.MemberRepository;
-import com.example.newserial.domain.member.repository.SocialMember;
-import com.example.newserial.domain.member.repository.SocialMemberRepository;
-import com.example.newserial.domain.member.service.AuthService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 // 로그인 성공시 자체적으로 access token과 refresh token 발급한다.
 // 그리고 기존 로그인 로직과 통합
 @Component
 @RequiredArgsConstructor
-public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
+@Slf4j
+public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
     private final JwtUtils jwtUtils;
 
 
@@ -30,16 +29,19 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException, ServletException {
         //oAuth2User 생성 (기존 로그인 userDetail과 동일)
+        log.info("oAuth2User 생성");
         CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
         Long id = oAuth2User.getId();
         String email = oAuth2User.getEmail();
 
         //토큰 생성
+        log.info("토큰 생성");
         TokenCarrier tokens = getTokensForResponse(oAuth2User);
         String refreshTokenCookie = tokens.getTokenCookie().toString();
         String accessToken = tokens.getAccessToken();
 
         //세션에 토큰과 쿠키 저장
+        log.info("토큰과 쿠키 저장");
         request.getSession().setAttribute("accessToken", accessToken);
         request.getSession().setAttribute("refreshCookie", refreshTokenCookie);
         request.getSession().setAttribute("id", id);
@@ -48,7 +50,8 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
 
 
         // /oauth2/redirect 로 이동
-        response.sendRedirect("/oauth2/redirect");
+        log.info("리다이렉트");
+        response.sendRedirect("/socialLogin?loginSuccess=true");
     }
 
     public TokenCarrier getTokensForResponse(CustomOAuth2User user) {
@@ -58,5 +61,11 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
                 .tokenCookie(refreshTokenCookie)
                 .accessToken(accessToken)
                 .build();
+    }
+
+    private String makeRedirectUrl(boolean loginSuccess) {
+        return UriComponentsBuilder.fromUriString("/socialLogin")
+                .queryParam("loginSuccess", loginSuccess)
+                .build().toUriString();
     }
 }
