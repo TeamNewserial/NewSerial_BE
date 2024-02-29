4c82611038a70bc88917c474e36fdffd04bf2a4a
aaa67 <enen9032@naver.com>
2024-02-29 18:43:32 +0900

    fix: 뉴스 조회 시 이미지 url 추가.

--- NewSerial/src/main/java/com/example/newserial/domain/news/dto/NewsListResponseDto.java
+++ NewSerial/src/main/java/com/example/newserial/domain/news/dto/NewsListResponseDto.java
@@ -9,18 +9,21 @@ public class NewsListResponseDto {
     private Integer category_id;
     private String title;
     private String body;
+    private String image;
 
     public NewsListResponseDto(News entity){
         this.id= entity.getId();
         this.category_id=entity.getCategory().getId();
         this.title=entity.getTitle();
         this.body=entity.getBody();
+        this.image= entity.getImageUrl();
     }
 
-    public NewsListResponseDto(Long id, Integer category_id, String title, String body) {
+    public NewsListResponseDto(Long id, Integer category_id, String title, String body, String image) {
         this.id = id;
         this.category_id = category_id;
         this.title = title;
         this.body = body;
+        this.image=image;
     }
 }
